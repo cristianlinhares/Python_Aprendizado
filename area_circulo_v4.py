448759ea6130a1f16eae7890ba10920279eadaf7
Cristian Linhares <64447432+cristianlinhares@users.noreply.github.com>
2020-05-06 22:58:02 -0300

    Add files via upload

---
+++ area_circulo_v4.py
@@ -0,0 +1,12 @@
+import math
+
+raio = 15.3
+
+area_circ = math.pi * (raio**2)
+
+print('Área do círculo =', area_circ)
+
+# se for importar o módulo math inteiro faça: import.math
+# se desejar importar apenas uma função de dentro do módulo,
+# como por exemplo o pi, faça: from math import pi
+# não esqueça de fazer as devidas alterações na fórmula de math.pi para pi.
